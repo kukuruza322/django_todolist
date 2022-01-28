f3a36fc65bc8d93bacf75419ef2602e0d26ea166
kukuruza322 <72981160+kukuruza322@users.noreply.github.com>
2022-01-28 20:57:15 +0300

    The next steps done : 1. python manage.py startapps base for create folder with prepared template of .py files needed for project.
    2. connect out App in "base" folder by adding BaseConfig in installed Apps list in settings.py
    
    2. \django_todolist\urls.py - was bound with base\urls.py by edit 'urlpatterns'
            and
    \Base\ : models, views, admin, urls, apps .py are changed to decribe Task objects and they relations with database and other Django elements.
    
    Models - description of our Task data in database, a "road" from Task to database table.
    Views - how the start page looks like
    URLS - ??? a route from user actions to what do next
    APPS - ??? apps description
    Admin - simply the description of what we can do in admin panel. Previously we add opportunity to read, write and manage Task instances (Models) in database.
    
    3. db created by -makemigrations and -migrate and all described in Models Task attributes have been recorded there. All changes are in 0001_initial.py
    
    4. some instances of Task were added in DB like Tasks using Admin panel for further editing and visualizing in view.py file (which manage representations of the main page not admin).
    
    5. Because of using class-based representation of view page we replace 'request' view by ListView (django sample) and set TaskList as a class not a function.
    
    6. added HTML templates and connections with TaskList and DetailList established

--- base/views.py
+++ base/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse  # we can delete this because of using of class-based view representation
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from .models import Task
 
-def taskList(request):
-    return HttpResponse('To Do List')
 
-# Create your views here.
+class TaskList(ListView):
+    model = Task
+    context_object_name = 'tasks'  # changing the name of object for HTML templates to 'tasks' and 'task'
+
+
+class TaskDetail(DetailView):
+    model = Task
+    context_object_name = 'task'
+    template_name = 'base/task.html' # allows to change task_detail to task.html
