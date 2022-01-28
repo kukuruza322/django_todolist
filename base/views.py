705f2f04717c844235fb9ce7d01804435e360d84
kukuruza322 <72981160+kukuruza322@users.noreply.github.com>
2022-01-28 21:48:05 +0300

    Task creation implemented. 1. In views class TaskCreate based on CreateView added 2. In urls link with HTML 'task-create' established 3. HTML template ready 4. Form can add new Task!

--- base/views.py
+++ base/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse  # we can delete this because of using of class-based view representation
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
 from .models import Task
 
 
@@ -14,3 +16,10 @@ class TaskDetail(DetailView):
     model = Task
     context_object_name = 'task'
     template_name = 'base/task.html' # allows to change task_detail to task.html
+
+
+class TaskCreate(CreateView):
+    model = Task
+    fields = '__all__'   # get list of all attributes of class Task or all Models values,
+                        # can be replaced by ['title', 'description', 'complete', 'created']
+    success_url = reverse_lazy('tasks')
