adb962d63bc119dfbc1cdb23d5176b94f05d6271
kukuruza322 <72981160+kukuruza322@users.noreply.github.com>
2022-01-29 22:56:42 +0300

    Deleting items works! 1. Import DeleteView and create class DeleteView as previously 2. Buttons "go back" added on all pages

--- base/views.py
+++ base/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse  # we can delete this because of using of class-based view representation
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .models import Task
 
@@ -27,4 +27,10 @@ class TaskCreate(CreateView):
 class TaskUpdate(UpdateView):
     model = Task
     fields = '__all__'
+    success_url = reverse_lazy('tasks')
+
+
+class DeleteView(DeleteView):
+    model = Task
+    content_object_name = 'task'
     success_url = reverse_lazy('tasks')
