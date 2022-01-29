2b4ce06e9b68d43345f7dd5bc1bb990fc1f303c0
kukuruza322 <72981160+kukuruza322@users.noreply.github.com>
2022-01-29 15:23:17 +0300

    Update(editing) items feature implemented 1. Upgrading views.py by additional import of UpdateView, create a class TaskUpdate 2. Edit urls by importing and adding TaskUpdate 3. Create a specify field for task editing in template task_list.html similarly as view-task functionality

--- base/views.py
+++ base/views.py
@@ -2,13 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse  # we can delete this because of using of class-based view representation
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 from django.urls import reverse_lazy
 from .models import Task
 
 
 class TaskList(ListView):
-    model = Task
+    model = Task                   # model = Task because of this is the name of the class (form) in models.py, and that's refers on all objects of it !
     context_object_name = 'tasks'  # changing the name of object for HTML templates to 'tasks' and 'task'
 
 
@@ -22,4 +22,9 @@ class TaskCreate(CreateView):
     model = Task
     fields = '__all__'   # get list of all attributes of class Task or all Models values,
                         # can be replaced by ['title', 'description', 'complete', 'created']
+    success_url = reverse_lazy('tasks')
+
+class TaskUpdate(UpdateView):
+    model = Task
+    fields = '__all__'
     success_url = reverse_lazy('tasks')
